5c921f40581f45f617082be7b56839361d16187c
Diogo Alves <diogoalvessilva86@gmail.com>
2017-08-02 23:55:05 -0300

    Removendo página de contato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { ListarMesasPage } from '../pages/listar-mesas/listar-mesas';
@@ -22,7 +21,6 @@ import { MockProvider } from '../providers/mock/mock';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     ListarMesasPage,
@@ -35,7 +33,6 @@ import { MockProvider } from '../providers/mock/mock';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     ListarMesasPage,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { ListarMesasPage } from '../listar-mesas/listar-mesas';
 
@@ -12,7 +11,6 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = AboutPage;
-  tab3Root = ContactPage;
   tab4Root = ListarMesasPage;
 
   constructor() {
